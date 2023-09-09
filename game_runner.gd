c055d56a338c75ec43d84659b9dfa2c842ae5fcf
pjz987 <pwj2012@gmail.com>
2023-09-08 17:49:30 -0700

    enough for today

--- game_runner.gd
+++ game_runner.gd
@@ -6,5 +6,16 @@ class_name GameRunner
 
 @export var grid: Grid
 
+var astar: AStarGrid2D
+
 func _ready():
-	print(grid)
+	print(grid.dimensions)
+	astar = AStarGrid2D.new()
+	astar.region = Rect2i(0, 0, grid.dimensions.x, grid.dimensions.y)
+	astar.cell_size = Vector2i(grid.cell_size, grid.cell_size)
+	print(astar.region, astar.cell_size)
+	
+func _draw():
+	for x in grid.dimensions.x:
+		for y in grid.dimensions.y:
+			draw_circle(Vector2(x, y) * grid.cell_size, grid.cell_size / 2, Color('red'))
